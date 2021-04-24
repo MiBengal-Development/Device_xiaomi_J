4e630c1b0b77b9df0130a8f285494fd6145b6fee
Demon000 <demonsingur@gmail.com>
2021-04-24 07:59:47 +0000

    juice: BoardConfig: Include Qcom Sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -174,6 +174,9 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.default
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_USERIMAGES_USE_F2FS := true
 
+# Sepolicy
+include device/qcom/sepolicy/SEPolicy.mk
+
 # Treble
 BOARD_VNDK_VERSION := current
 
