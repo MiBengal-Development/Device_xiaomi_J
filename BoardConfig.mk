ee239e691e36cfb6caa24e821005161c151ad240
Joboy Jhem Francisco <joboyjhem24@gmail.com>
2021-05-01 04:09:57 +0000

    juice: Include Private Sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -176,6 +176,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 
 # Sepolicy
 include device/qcom/sepolicy/SEPolicy.mk
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
 # Treble
 BOARD_VNDK_VERSION := current
