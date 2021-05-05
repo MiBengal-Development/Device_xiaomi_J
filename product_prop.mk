11403f511406ac3564aadf3262666e27b782725f
karthik558 <karthik.lal558@gmail.com>
2021-05-05 17:38:04 +0700

    Juice: prop: Add props to improve battery backup

--- product_prop.mk
+++ product_prop.mk
@@ -15,3 +15,11 @@ PRODUCT_PRODUCT_PROPERTIES += \
     debug.hwui.renderer=skiavk \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     debug.sf.latch_unsignaled=0
+
+# Power-saving props
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.use_data_netmgrd=true \
+    ro.vold.umsdirtyratio=20 \
+    ro.ril.disable.power.collapse=0 \
+    power.saving.mode=1 \
+    pm.sleep_mode=1
