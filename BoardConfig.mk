850e4f55d2c515adc96d81df5573a1ac7f6e23cf
Demon000 <demonsingur@gmail.com>
2021-06-15 23:12:05 +0000

    juice: BoardConfig: use dexpreopt on linux host and non-eng build

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,6 +62,16 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 # Display
 TARGET_USES_HWC2 := true
 
+# Dex
+ifeq ($(HOST_OS),linux)
+    ifneq ($(TARGET_BUILD_VARIANT),eng)
+        ifeq ($(WITH_DEXPREOPT),)
+            WITH_DEXPREOPT := true
+            WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+        endif
+    endif
+endif
+
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
