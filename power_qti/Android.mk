319c4292af14fbdd279ed5ba770755822520bfd7
I-n-o-k <inok.dr189@gmail.com>
2021-07-27 09:22:19 +0700

    juice: Build QTI Power Service

---
+++ power_qti/Android.mk
@@ -0,0 +1,48 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := android.hardware.power-service-qti-juice
+LOCAL_MODULE_TAGS  := optional
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/bin
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE_STEM := android.hardware.power-service-qti
+
+LOCAL_SHARED_LIBRARIES := \
+    liblog \
+    libcutils \
+    libdl \
+    libbase \
+    libutils \
+    android.hardware.power-ndk_platform \
+    libbinder_ndk
+
+LOCAL_HEADER_LIBRARIES := \
+    libhardware_headers
+
+LOCAL_SRC_FILES := \
+    power-common.c \
+    metadata-parser.c \
+    utils.c \
+    list.c \
+    hint-data.c \
+    Power.cpp \
+    main.cpp
+
+LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE="/sys/touchpanel/double_tap"
+
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := android.hardware.power-service-qti-juice.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init
+LOCAL_MODULE_STEM := android.hardware.power-service-qti.rc
+
+LOCAL_SRC_FILES := android.hardware.power-service-qti-juice.rc
+
+include $(BUILD_PREBUILT)
