14498356066232da04c4677aa2e2c0a26e6ad575
I-n-o-k <inok.dr189@gmail.com>
2021-08-06 21:16:56 +0700

    juice: import rootdir file from stock

--- device.mk
+++ device.mk
@@ -106,6 +106,39 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.default:$(TARGET_COPY_OUT_RAMDISK)/fstab.default
 
+# Ramdisk
+PRODUCT_PACKAGES += \
+    getwlansar.sh \
+    init.class_main.sh \
+    init.crda.sh \
+    init.mdm.sh \
+    init.mi.usb.sh \
+    init.qcom.class_core.sh \
+    init.qcom.coex.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.efs.sync.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.sdio.sh \
+    init.qcom.sensors.sh \
+    init.qcom.sh \
+    init.qcom.usb.sh \
+    init.qti.chg_policy.sh \
+    init.qti.dcvs.sh \
+    init.qti.qcv.sh \
+    install-recovery.sh \
+    qca6234-service.sh \
+    setwlansarhigh.sh \
+    setwlansarlow.sh \
+    init.qcom.factory.rc \
+    init.qcom.rc \
+    init.qcom.usb.rc \
+    init.qti.ufs.rc \
+    init.stnfc.rc \
+    init.target.rc \
+    fstab.default \
+    fstab.emmc \
+    ueventd.qcom.rc
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl \
